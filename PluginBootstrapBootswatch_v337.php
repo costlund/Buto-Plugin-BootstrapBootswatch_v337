2600ade4cbcb8e99ada6567868b8e8077b54f639
Claes Östlund <claes@stenbergit.se>
2017-10-17 10:06:06 +0200

    Improvements.

--- PluginBootstrapBootswatch_v337.php
+++ PluginBootstrapBootswatch_v337.php
@@ -13,18 +13,20 @@ class PluginBootstrapBootswatch_v337{
   #code#
   */
   public static function widget_include($data){
-    $element = array();
     /**
-     * Set other theme in session.
+     * Set other theme in session via querystring.
      */
     if(wfRequest::get('bootstrap_bootswatch_v337_theme')){
+      /**
+       * Check if the theme exist.
+       */
       $availible = wfSettings::getSettings('/plugin/bootstrap/bootswatch_v337/theme/availible.yml');
-      if(array_search(wfRequest::get('bootstrap_bootswatch_v337_theme'), $availible)){
+      if(array_search(wfRequest::get('bootstrap_bootswatch_v337_theme'), $availible)!==false){
         $_SESSION['plugin']['bootstrap']['bootswatch_v337']['theme'] = wfRequest::get('bootstrap_bootswatch_v337_theme');
       }
     }
     /**
-     * 
+     * Set theme.
      */
     if(isset($_SESSION['plugin']['bootstrap']['bootswatch_v337']['theme'])){
       /**
@@ -33,7 +35,7 @@ class PluginBootstrapBootswatch_v337{
       $data['data']['theme'] = $_SESSION['plugin']['bootstrap']['bootswatch_v337']['theme'];
     }elseif(!isset($data['data']['theme'])){
       /**
-       * If not set in widget.
+       * If not set in widget we set Cerulean as default theme.
        */
       $data['data']['theme'] = 'Cerulean';
     }else{
@@ -42,8 +44,13 @@ class PluginBootstrapBootswatch_v337{
        */
     }
     /**
+     * Set current theme to pic upp in selectbox widget.
+     */
+    wfArray::set($GLOBALS, 'sys/settings/plugin/bootstrap/bootswatch_v337/current_theme', $data['data']['theme']);
+    /**
      * Create element and render.
      */
+    $element = array();
     $element[] = wfDocument::createHtmlElement('link', null, array('href' => '/plugin/bootstrap/bootswatch_v337/'.strtolower($data['data']['theme']).'/bootstrap.min.css', 'rel' => 'stylesheet'));
     wfDocument::renderElement($element);
   }
@@ -57,6 +64,10 @@ class PluginBootstrapBootswatch_v337{
      */
     $settings = new PluginWfArray($GLOBALS['sys']['settings']);
     /**
+     * Current theme.
+     */
+    $current_theme = wfArray::get($GLOBALS, 'sys/settings/plugin/bootstrap/bootswatch_v337/current_theme');
+    /**
      * Create select.
      */
     $select = wfDocument::createHtmlElementAsObject('select');
@@ -65,7 +76,11 @@ class PluginBootstrapBootswatch_v337{
     $option = array();
     $option[] = wfDocument::createHtmlElement('option', '', array('value' => ''));
     foreach ($availible as $key => $value) {
-      $option[] = wfDocument::createHtmlElement('option', $value, array('value' => $value));
+      $attribute = array('value' => $value);
+      if($value == $current_theme){
+        $attribute = array_merge($attribute, array('selected' => 'selected'));
+      }
+      $option[] = wfDocument::createHtmlElement('option', $value, $attribute);
     }
     $select->set('innerHTML', $option);
     /**
